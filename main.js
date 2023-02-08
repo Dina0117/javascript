745c3b5bc3e27244be696e1347730f5a971e832f
Madina Burieva <97452334+Dina0117@users.noreply.github.com>
2023-02-08 21:04:58 +0900

    object1

---
+++ main.js
@@ -0,0 +1,40 @@
+
+// let user={
+//     name: 'Jhon',
+//     surname:'Smith'
+// };
+// user.name='Pete'
+// console.log(user);
+//=================//
+var schedule = {};
+Object.prototype._isEmpty = function(){
+    console.log(Object.keys(schedule)?.length==0);
+}
+Object._isEmpty();
+
+
+// var schedule= {"8:30":'get up'};
+// Object.prototype._isEmpty= function(){
+//     console.log(Object.keys(schedule)?.length==0);
+// }
+// Object._isEmpty();
+//=====================
+
+// let salaries={
+//     John:100,
+//     Ann:160,
+//     Pete:130,
+//     dd:'dd'
+// };
+// sum=0;
+// for(let i in salaries){
+//     if(salaries.hasOwnProperty(i)){
+        
+//         sum+=salaries[i]
+//  }
+// }
+//     console.log(sum); 
+
+//========================
+
+
